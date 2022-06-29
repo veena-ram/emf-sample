11c252b69accb6f1a171c2b300360709d69391f5
Veena Ram <veenaram92@gmail.com>
2022-06-29 12:13:00 -0400

    Initial Commit

---
+++ emf-sample/emf_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  emf_sampleApp.swift
+//  emf-sample
+//
+//  Created by Veena Ram on 6/29/22.
+//
+
+import SwiftUI
+
+@main
+struct emf_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ emf-sampleUITests/emf_sampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  emf_sampleUITestsLaunchTests.swift
+//  emf-sampleUITests
+//
+//  Created by Veena Ram on 6/29/22.
+//
+
+import XCTest
+
+class emf_sampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
